7fc511d2f073658c37c6cacce8813524e00a711b
NeiltonSeguins <seguins.neilton@gmailcom>
2024-11-22 15:51:14 -0300

    Vídeo 5.2 - Criando o gráfico de pizza

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -10,6 +10,7 @@ import {
   ArcElement,
   Tooltip,
   Legend,
+  plugins,
 } from "chart.js";
 
 import { Pie } from "react-chartjs-2";
@@ -30,12 +31,44 @@ export const AreaChart = styled.div`
 `;
 
 const BalancoFinanceiro = () => {
+  const data = {
+    datasets: [
+      {
+        label: "Gastos por categoria",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  };
+
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
       <CartaoCorpo>
         <AreaChart>
-          <Pie />
+          <Pie data={data} options={options} />
         </AreaChart>
       </CartaoCorpo>
     </Cartao>
